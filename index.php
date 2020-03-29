28ae1e1f3e6af78ae1499ef8a033d1a28d417777
Nenad Mitrovic <nenadmitrovic04@gmail.com>
2020-03-29 23:54:19 +0200

    add structure and styling for index page

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-Hello Doodle!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Doodle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"></link>
+    
+</head>
+<body>
+
+
+
+<div class="wrapper indexPage">
+
+    <div class="mainSection">
+
+
+    <div class="logoContainer">
+        <img src="assets/img/noogle.png" alt="">
+    </div>
+
+    <div class="searchContainer">
+        <form action="search.php" method="GET">
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="Search">
+        </form>
+    </div>
+
+    </div>
+
+
+
+</div>
+
+
+
+    
+</body>
+</html>
